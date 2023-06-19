53bd144189b62536029d88785603ea724951b6d8
cardinalkdk752 <101612070+cardinalkdk752@users.noreply.github.com>
2023-06-20 00:23:03 +0900

    Add files via upload

--- homework01/game.c
+++ homework01/game.c
@@ -5,6 +5,30 @@
 #include <conio.h>
 #include <mmsystem.h>
 
+char menu[465];
+void viewCursor() {
+        HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO cursorInfo;
+        cursorInfo.dwSize = 1;
+        cursorInfo.bVisible = FALSE;
+        SetConsoleCursorInfo(consoleHandle, &cursorInfo);
+}
+void writeStringToBuffer(const char* string, int x, int y)
+{
+	int i=0;
+	while (i<strlen(string))
+	{
+        system("cls");
+        viewCursor();
+		menu[i+x+31*y]=string[i];
+		printf("%s\n", menu);
+        Sleep(16);
+		i++;
+	}
+}
+void setThemeColor(unsigned textColor, unsigned backColor) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), textColor | (backColor << 4));
+}
 #define MAP_WIDTH 30
 #define MAP_HEIGHT 15
 
@@ -19,13 +43,6 @@ char map[MAP_HEIGHT][MAP_WIDTH];
 int progress = 0;
 int playtime = 0;
 
-void hideCursor() {
-        HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_CURSOR_INFO cursorInfo;
-        cursorInfo.dwSize = 1;
-        cursorInfo.bVisible = FALSE;
-        SetConsoleCursorInfo(consoleHandle, &cursorInfo);
-}
 void clearMap() {
     for (int i = 0; i < MAP_HEIGHT; i++) {
         for (int j = 0; j < MAP_WIDTH; j++) {
@@ -52,27 +69,8 @@ void drawMap() {
     }
 }
 
-void moveCharacter(int direction) {
-    int newX = character.x;
-    int newY = character.y;
 
-    if (direction == -1) {
-        newX--;
-    } else if (direction == 1) {
-        newX++;
-    } else if (direction == 0) {
-        newY--;
-    } else if (direction == 2 && character.y < MAP_HEIGHT - 2) {
-        newY++;
-    }
 
-    if (newX >= 0 && newX < MAP_WIDTH && newY >= 0 && newY < MAP_HEIGHT && map[newY][newX] != '#') {
-        map[character.y][character.x] = ' ';
-        character.x = newX;
-        character.y = newY;
-        map[character.y][character.x] = '@';
-    }
-}
 
 int mineOre() {
     int progress = 0;
@@ -117,6 +115,32 @@ int mineOre() {
     return progress;
 }
 
+void moveCharacter(int direction) {
+    int newX = character.x;
+    int newY = character.y;
+
+    if (direction == -1) {
+        newX--;
+    } else if (direction == 1) {
+        newX++;
+    } else if (direction == 0) {
+        newY--;
+    } else if (direction == 2 && character.y < MAP_HEIGHT - 2) {
+        newY++;
+    } else if (direction == 3) {
+        // 스페이스바 입력 처리
+        int progress = mineOre();
+        // 여기에서 진행도를 처리하는 로직을 추가할 수 있습니다.
+        return;
+    }
+
+    if (newX >= 0 && newX < MAP_WIDTH && newY >= 0 && newY < MAP_HEIGHT && map[newY][newX] != '#') {
+        map[character.y][character.x] = ' ';
+        character.x = newX;
+        character.y = newY;
+        map[character.y][character.x] = '@';
+    }
+}
 void initializeGame() {
     srand(time(NULL));
 
@@ -192,13 +216,17 @@ void generateRandomBlocks() {
 
 
 void updateBlockGeneration() {
-    int timeElapsed = playtime; // 플레이 시간으로 간격 조절
+    int timeElapsed = playtime / 3;
 
     int blocksPerGeneration;
-    if (timeElapsed % 3 == 0) { // 3초마다 블록 변경
-        blocksPerGeneration = timeElapsed / 3;
+    if (timeElapsed < 1) {
+        blocksPerGeneration = 1;
+    } else if (timeElapsed < 3) {
+        blocksPerGeneration = 2;
+    } else if (timeElapsed < 5) {
+        blocksPerGeneration = 3;
     } else {
-        blocksPerGeneration = 0;
+        blocksPerGeneration = 5;
     }
 
     for (int i = 0; i < blocksPerGeneration; i++) {
@@ -230,6 +258,34 @@ void updateBlockGeneration() {
         map[MAP_HEIGHT - 1][randomX] = oreSymbol;
     }
 }
+int fixed_screen()
+{
+	int i=0;
+	// 가로가 0부터 30까지 칸은 총 31개 31번째는 계행문자
+	
+	while(i<465)
+	{
+		int x=i%31;
+		int y=i/31;
+
+		if(x==30)
+		{
+			menu[i]='\n';
+		}
+		else if(x==0 || x==29 || y==0 || y==14)
+		{
+			menu[i]='#';
+		}
+		else
+		{
+			menu[i]=' ';
+		}
+		i=i+1;
+	}
+	menu[464]='\0';
+	return 0;
+}
+
 void playMusic() {
     PlaySound("main2sound.wav", NULL, SND_FILENAME | SND_ASYNC);
 }
@@ -237,64 +293,175 @@ void playMusic() {
 void stopMusic() {
     PlaySound(NULL, NULL, 0);
 }
-
+#define true 1
+#define false 0
 int main() {
-    initializeGame();
-    generateRandomBlocks();
-    hideCursor();
-    clock_t previousTime = clock();
-    float frameDelay = 0.016f;
-    float accumulatedTime = 0.0f;
-    float playtimeTimer = 0.0f;
-    const float playtimeDelay = 1.0f;
-    playMusic();
-    while (progress < 100) {
-        clock_t currentTime = clock();
-        float deltaTime = (float)(currentTime - previousTime) / CLOCKS_PER_SEC;
-        previousTime = currentTime;
+    clock_t start, end;
+    start = clock();
+    int cpu_time;
+    int inputValue = 0;
+    int game_fk = 1;
+    int game_play = 1;
+    setThemeColor(15, 0);
+    fixed_screen();
+    writeStringToBuffer("Loading...", 6, 6);
+    Sleep(2000);
 
-        accumulatedTime += deltaTime;
-        playtimeTimer += deltaTime;
+    while (game_fk) {
+        setThemeColor(15, 0);
+        fixed_screen();
 
-        if (accumulatedTime >= frameDelay) {
-            int input;
-            if (_kbhit()) {
-                input = _getch();
-                if (input == 'q') {
-                    break;
-                } else if (input == 'a') {
-                    moveCharacter(-1);
-                } else if (input == 'd') {
-                    moveCharacter(1);
-                } else if (input == 'w') {
-                    moveCharacter(0);
-                } else if (input == 's') {
-                    moveCharacter(2);
+        if (game_fk == 1) {
+            writeStringToBuffer("Tungsten", 9, 2);
+            writeStringToBuffer("1. Start Game", 5, 7);
+            writeStringToBuffer("2. How to Play", 5, 8);
+            writeStringToBuffer("3. Exit", 5, 9);
+        }
+        else if (game_fk == 2) {
+            writeStringToBuffer("Dig up the ore", 6, 6);
+            writeStringToBuffer("Press the WASD Key to move", 2, 8);
+            writeStringToBuffer("Return to menu?", 8, 11);
+            writeStringToBuffer("1. Yes 2. No", 10, 12);
+        }
+        else if (game_fk == 3) {
+            writeStringToBuffer("Are you sure Exit?", 6, 6);
+            writeStringToBuffer("1. Yes 2. No", 10, 7);
+        }
+
+        scanf("%d", &inputValue);
+        system("cls");
+
+        // 메인메뉴에서 입력을 처리함
+        if (game_fk == 1) {
+            if (inputValue == 1) {
+                // 게임 실행
+                initializeGame();
+                generateRandomBlocks();
+                viewCursor();
+                clock_t previousTime = clock();
+                float frameDelay = 0.016f;
+                float accumulatedTime = 0.0f;
+                float playtimeTimer = 0.0f;
+                const float playtimeDelay = 1.0f;
+                playMusic();
+
+                while (progress < 100) {
+                    clock_t currentTime = clock();
+                    float deltaTime = (float)(currentTime - previousTime) / CLOCKS_PER_SEC;
+                    previousTime = currentTime;
+
+                    accumulatedTime += deltaTime;
+                    playtimeTimer += deltaTime;
+
+                    if (accumulatedTime >= frameDelay) {
+                        int input;
+                        if (_kbhit()) {
+                            input = _getch();
+                            if (input == 'q') {
+                                break;
+                            }
+                            else if (input == 'a') {
+                                moveCharacter(-1);
+                            }
+                            else if (input == 'd') {
+                                moveCharacter(1);
+                            }
+                            else if (input == 'w') {
+                                moveCharacter(0);
+                            }
+                            else if (input == 's') {
+                                moveCharacter(2);
+                            }else if (input == ' ') {
+                                moveCharacter(3);  // 스페이스바 입력
+                            }
+                        }
+
+                        if (playtimeTimer >= playtimeDelay) {
+                            playtime++;
+                            playtimeTimer = 0.0f;
+                        }
+
+                        progress += mineOre();
+                        updateBlockGeneration();
+
+                        drawMap();
+                        printf("Progress of assignment: %d/100\n", progress);
+                        printf("Time to do homework: %d sec\n", playtime);
+
+                        accumulatedTime -= frameDelay;
+                    }
+                }
+
+                stopMusic();
+                printf("final examinations!!!!!\n");
+                printf("Playtime: %d sec\n", playtime);
+                setThemeColor(15, 0);
+
+                while (true) {
+                    printf("Return to game? (1. Yes 2. No 3. Go to main)\n");
+                    scanf("%d", &inputValue);
+                    system("cls");
+
+                    if (inputValue == 1) {
+                        // 게임 재실행
+                        // 추가된 부분: 게임 초기화 및 블록 생성
+                        initializeGame();
+                        generateRandomBlocks();
+                        viewCursor();
+                        clock_t previousTime = clock();
+                        float frameDelay = 0.016f;
+                        float accumulatedTime = 0.0f;
+                        float playtimeTimer = 0.0f;
+                        const float playtimeDelay = 1.0f;
+                        progress = 0; // 게임 진행도 초기화
+                        break;
+                    }
+                    else if (inputValue == 2) {
+                        // 완전히 종료
+                        game_fk = 0;
+                        break;
+                    }
+                    else if (inputValue == 3) {
+                        // 메인화면으로 돌아감
+                        game_fk = 1;
+                        break;
+                    }
+                    else {
+                        printf("Invalid input. Please try again.\n");
+                    }
                 }
             }
-
-            if (playtimeTimer >= playtimeDelay) {
-                playtime++;
-                playtimeTimer = 0.0f;
+            else if (inputValue == 2) {
+                game_fk = 2;
             }
-
-            progress += mineOre();
-            updateBlockGeneration();
-
-            drawMap();
-            printf("Progress: %d/100\n", progress);
-            printf("Playtime: %d sec\n", playtime);
-
-            accumulatedTime -= frameDelay;
+            else if (inputValue == 3) {
+                game_fk = 3;
+            }
+        }
+        else if (game_fk == 2) {
+            if (inputValue == 1)
+                game_fk = 1;
+            else if (inputValue == 2)
+                game_fk = 2;
+            else
+                game_fk = 2;
+        }
+        else if (game_fk == 3) {
+            if (inputValue == 1) {
+                fixed_screen();
+                writeStringToBuffer("Game Exit...", 6, 5);
+                Sleep(2000);
+                game_fk = 0;
+            }
+            else if (inputValue == 2)
+                game_fk = 1;
+            else
+                game_fk = 3;
         }
     }
-    stopMusic();
-    printf("Game Clear!\n");
-    printf("Playtime: %d sec\n", playtime);
 
     return 0;
 }
-
 
 
 
--- homework01/screen.c
+++ homework01/screen.c
@@ -1,167 +1,146 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
 #include <time.h>
 
-#ifdef _WIN32
-#include <Windows.h>
-#endif
-#define WIDTH 31
-#define HEIGHT 15
-#define _CRT_SECURE_NO_WARNINGS
 
-void clearScreen() {
-#ifdef _WIN32
-    system("cls");
-#else
-    printf("\033[2J\033[H");
-#endif
+char menu[465];
+void writeStringToBuffer(const char* string, int x, int y)
+{
+	int i=0;
+	while (i<strlen(string))
+	{
+		Sleep(16);
+		menu[i+x+31*y]=string[i];
+		printf("%s\n", menu);
+		i++;
+	}
 }
-
-void clearBuffer(char* screenBuf, int width, int height) {
-    int i, j;
-    for (j = 0; j < height; j++) {
-        for (i = 0; i < width; i++) {
-            if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
-                screenBuf[i + (j * width)] = '#';
-            } else {
-                screenBuf[i + (j * width)] = ' ';
-            }
-        }
-        screenBuf[(j + 1) * width - 2] = '#';
-        screenBuf[(j + 1) * width - 1] = '\n';
-    }
-    screenBuf[width * height - 2] = '#';
-    screenBuf[width * height - 1] = '\0';
-    clearScreen();
+void setConsoleColor(unsigned textColor, unsigned backColor) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), textColor | (backColor << 4));
 }
+int fixed_screen()
+{
+	int i=0;
+	// 가로가 0부터 30까지 칸은 총 31개 31번째는 계행문자
+	
+	while(i<465)
+	{
+		int x=i%31;
+		int y=i/31;
 
-void writeStringToBufferAnimated(const char* string, char* screenBuf, int width, int height, int x, int y) {
-    int len = strlen(string);
-    int i;
-
-    if (x < 0 || x + len >= width || y < 0 || y >= height) {
-        return;
-    }
-
-    for (i = 0; i < len; i++) {
-        screenBuf[x + i + (y * width)] = string[i];
-    }
-
-    Sleep(100);
+		if(x==30)
+		{
+			menu[i]='\n';
+		}
+		else if(x==0 || x==29 || y==0 || y==14)
+		{
+			menu[i]='#';
+		}
+		else
+		{
+			menu[i]=' ';
+		}
+		i=i+1;
+	}
+	menu[464]='\0';
+	return 0;
 }
 
 
-void setLoadingToScreenBufferAnimated(char* screenBuf, int width, int height) {
-    clearBuffer(screenBuf, width, height);
-    writeStringToBufferAnimated("Loading...", screenBuf, width, height, 9, 8);
-}
+int main()
+{
+	clock_t start,end;
+	start=clock();
+	int cpu_time;
+	int inputValue = 0;
+	int game_state = 1;
+	int game_play =1;
+	setConsoleColor(15,0);
+	fixed_screen();
+	writeStringToBuffer("Loding...",6,6);
+	Sleep(2000);
 
-void setTitleToScreenBuffer(char* screenBuf, int width, int height) {
-    clearBuffer(screenBuf, width, height);
-    writeStringToBufferAnimated("Tungsten", screenBuf, width, height, 10, 3);
-    writeStringToBufferAnimated("1.Play Game", screenBuf, width, height, 3, 6);
-    writeStringToBufferAnimated("2.How To Play?", screenBuf, width, height, 3, 9);
-    writeStringToBufferAnimated("3.Exit", screenBuf, width, height, 3, 12);
-}
-void setsc1ToScreenBuffer(char* screenBuf, int width, int height) {
-    clearBuffer(screenBuf, width, height);
+	while(game_state)
+	{
+		
+		
+		setConsoleColor(15,0);
+		fixed_screen();
 
-}
+		
+		if(game_state == 1){
+			writeStringToBuffer("Eat Survival",9,2);
+			writeStringToBuffer("V.0.1",12,3);
+			writeStringToBuffer("1. Start Game",5,7);
+			writeStringToBuffer("2. How to Play",5,8);
+			writeStringToBuffer("3. Exit",5,9);
+		}
+		else if(game_state == 2){
+			writeStringToBuffer("Survive from hunger",6,6);
+			writeStringToBuffer("Press the arrow key to move",1,8);
+			writeStringToBuffer("Retrun to menu?",8,11);
+			writeStringToBuffer("1.yes 2.no",10,12);
+		}
+		else if(game_state == 3){
+			writeStringToBuffer("Are you sure Exit?",6,6);
+			writeStringToBuffer("1.yes 2.no",10,7);
+		}
+		
+		scanf("%d", &inputValue);
+		system("cls");
+		
+		
+		
+		// 메인메뉴에서 입력을 처리함
+		if (game_state == 1){
+			if (inputValue == 1){
+				system("cls");
+				drawMap();
+				system("cls");
+				setConsoleColor(15,0);
+				game_state=1;
+				
+			}
+				
+			else if (inputValue == 2)
+				game_state = 2;
+			else if (inputValue == 3)
+				game_state = 3;
+		}
+		else if (game_state == 2){
+			if (inputValue == 1)
+				game_state = 1;
+			else if(inputValue==2)
+				game_state = 2;
+			else
+				game_state =2;
+				
+		}
+		else if (game_state == 3){
+			if (inputValue == 1){
+				fixed_screen();
+				end = clock();
+				cpu_time=((int)(end-start)/CLOCKS_PER_SEC);
+				char time[20];
+				sprintf(time,"%d",cpu_time);
+				writeStringToBuffer("Program execution time",3,6);
+				writeStringToBuffer(time,25,6);
+				Sleep(1000);
+				writeStringToBuffer("Exit...",6,9);
+				Sleep(2000);
+				game_state = 0;
+			}
+			else if (inputValue == 2)
+				game_state = 1;
+			else
+				game_state =3;
+		}
 
-
-
-void setsc2ToScreenBuffer(char* screenBuf, int width, int height) {
-    clearBuffer(screenBuf, width, height);
-    writeStringToBufferAnimated("How To Play Game?", screenBuf, width, height, 6, 3);
-    writeStringToBufferAnimated("Press your space bar", screenBuf, width, height, 2, 7);
-    writeStringToBufferAnimated("to be able to dig up ore", screenBuf, width, height, 2, 8);
-    writeStringToBufferAnimated("Press 4 to back", screenBuf, width, height, 5, 9);
-}
-
-void setsc3ToScreenBuffer(char* screenBuf, int width, int height) {
-    clearBuffer(screenBuf, width, height);
-    writeStringToBufferAnimated("Are you really going to", screenBuf, width, height, 3, 3);
-    writeStringToBufferAnimated("end the game?", screenBuf, width, height, 3, 4);
-    writeStringToBufferAnimated("1. Yes", screenBuf, width, height, 3, 8);
-    writeStringToBufferAnimated("2. No", screenBuf, width, height, 3, 9);
-}
-
-
-
-#ifdef _YUHAN_C_WINDOW_USER
-void showLoadingScreen() {
-    clock_t start = clock();
-    int i;
-
-    char screenBuf[WIDTH * HEIGHT + 1];
-    setLoadingToScreenBufferAnimated(screenBuf, WIDTH, HEIGHT);
-
-    for (i = 0; i < 30; i++) {
-        COORD pos = { 0, 0 };
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-        printf("%s\n로딩 중... %d%%", screenBuf, (i + 1) * 100 / 30);
-        Sleep(100);
-    }
-
-    clock_t end = clock();
-    printf("\n로딩이 완료되었습니다.\n");
-    printf("로딩 시간: %dms\n", (int)(end - start));
-}
-#endif
-
-int main(void) {
-    char screen[WIDTH * HEIGHT + 1];
-    int input;
-    int currentState = 0;
-
-#ifdef _YUHAN_C_WINDOW_USER
-    showLoadingScreen();
-#else
-    setLoadingToScreenBufferAnimated(screen, WIDTH, HEIGHT);
-    printf("%s\n", screen);
-#endif
-
-    setTitleToScreenBuffer(screen, WIDTH, HEIGHT);
-
-    while (1) {
-        printf("%s\npress key-->\n", screen);
-        scanf("%d", &input);
-
-        if (currentState == 0) {
-            if (input == 1) {
-                setsc1ToScreenBuffer(screen, WIDTH, HEIGHT);
-                currentState = 1;
-            } else if (input == 2) {
-                setsc2ToScreenBuffer(screen, WIDTH, HEIGHT);
-                currentState = 2;
-            } else if (input == 3) {
-                setsc3ToScreenBuffer(screen, WIDTH, HEIGHT);
-                currentState = 3;
-            }
-        } else if (currentState == 1) {
-            if (input == 4) {
-                setTitleToScreenBuffer(screen, WIDTH, HEIGHT);
-                currentState = 0;
-            }
-        } else if (currentState == 2) {
-            if (input == 4) {
-                setTitleToScreenBuffer(screen, WIDTH, HEIGHT);
-                currentState = 0;
-            }
-        } else if (currentState == 3) {
-            if (input == 1) {
-                break;
-            } else if (input == 2) {
-                setTitleToScreenBuffer(screen, WIDTH, HEIGHT);
-                currentState = 0;
-            }
-        }
-    }
-
-    printf("Program terminated.\n");
-
-    return 0;
+	}
+    
+	return 0;
 }
 
 
